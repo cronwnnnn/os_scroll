040a8b3073b41176de9744ebf12b4d3084096323
crownnnn <984739885@qq.com>
2026-03-24 15:34:46 +0800

    feat: add bool and true flase null typrdef

--- src/include/common/types.h
+++ src/include/common/types.h
@@ -1,12 +1,18 @@
 #ifndef COMMON_TYPE_H
 #define COMMON_TYPE_H
 
+#define nullptr ((void*)0)
+#define true 1
+#define false 0
+
 typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef short int16_t;
 typedef unsigned short uint16_t;
 typedef char int8_t;
 typedef unsigned char uint8_t;
+
+typedef unsigned char bool;
 
 
 #endif // COMMON_TYPE_H
